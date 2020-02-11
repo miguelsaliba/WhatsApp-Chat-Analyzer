a0201ffb77115d49e61652ecc82167c0de4e3197
miguelsaliba <36637899+miguelsaliba@users.noreply.github.com>
2020-02-11 11:39:07 +0300

    added comments

--- Chat Analyzer.py
+++ Chat Analyzer.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import re
 
+# file path to txt file
 f = open("chat.txt", "r", encoding="utf8")
+
+# skips first line which is the end to end encryption message
 firstline = f.readline()
 
 count = 0.0
@@ -11,10 +14,12 @@ maxday = ""
 currentday = ""
 currentcount = 0
 
+# removes emojis from names
 def deEmojify(inputString):
     return inputString.encode('ascii', 'ignore').decode('ascii')
 
 for x in f:
+    # if the day changes it recents the day counter and checks if its more than the max day
     if (x[x.find("[")+1 : x.find(",")] != currentday):
         if currentcount > maxperday:
             maxperday = currentcount
@@ -23,17 +28,22 @@ for x in f:
         currentday = x[x.find("[")+1 : x.find(",")]
         currentcount = 0
 
+    # removes the date and time
     string = (x[x.find("]")+2:])
 
 
+    # checks if its a message and not just a new line
     if re.search(".+/.+/..*:..:..", x) and ": " in string:
+        # check if theres a new person
         if (string.split(": ")[0] not in people):
             people[string.split(": ", 1)[0]] = 0
 
+        # adds message to person
         people[string.split(": ")[0]] += 1
 
 
         count += 1
+        # counter for one day
         currentcount += 1
 
 print("Total: {0:,g}".format(count))
